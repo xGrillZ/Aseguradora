2e7e121524b63c2e61f779c7ee346c95811cc90d
RANDALL\randall <randallab_@outlook.com>
2020-11-15 19:13:37 -0600

    Adding Modelos
    Adding Modelos

--- AseguradoraFinal/AseguradoraFinal/Modelos/ClientesBDModel.Designer.cs
+++ AseguradoraFinal/AseguradoraFinal/Modelos/ClientesBDModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'R:\Progra V\WFApplication\WFApplication\Modelos\ClientesBDModel.edmx'. 
+﻿// T4 code generation is enabled for model 'R:\Git\Aseguradora\AseguradoraFinal\AseguradoraFinal\Modelos\ClientesBDModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
