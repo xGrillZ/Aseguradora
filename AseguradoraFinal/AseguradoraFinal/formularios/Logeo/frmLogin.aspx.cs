1c9dfe37bc3180db7d89229598a6522161c50168
xGrillZ <stevenalonso99@hotmail.com>
2020-12-02 11:21:06 -0600

    Sistema Logeo
    Inserción del sistema de Logeo base

---
+++ AseguradoraFinal/AseguradoraFinal/formularios/Logeo/frmLogin.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AseguradoraFinal.Modelos;
+
+namespace AseguradoraFinal.formularios.Logeo
+{
+    public partial class frmLogin : System.Web.UI.Page
+    {
+        aseguradorarjsEntities modeloBD = new aseguradorarjsEntities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btAceptar_Click(object sender, EventArgs e)
+        {
+            ///Validación correcta del formulario
+            if (this.Page.IsValid)
+            {
+                this.RealizarAutenticacion();
+            }
+        }
+
+        void RealizarAutenticacion()
+        {
+
+        }
+    }
+}
