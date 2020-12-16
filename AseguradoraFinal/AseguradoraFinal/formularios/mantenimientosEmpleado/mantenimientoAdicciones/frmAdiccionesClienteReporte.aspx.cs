e262bce44d0ff0969e41938bebd47a686f74b902
RANDALL\randall <randallab_@outlook.com>
2020-12-16 12:04:04 -0600

    issues on frm reports
    issues on frm reports with PAs

--- AseguradoraFinal/AseguradoraFinal/formularios/mantenimientosEmpleado/mantenimientoAdicciones/frmAdiccionesClienteReporte.aspx.cs
+++ AseguradoraFinal/AseguradoraFinal/formularios/mantenimientosEmpleado/mantenimientoAdicciones/frmAdiccionesClienteReporte.aspx.cs
@@ -13,5 +13,58 @@ namespace AseguradoraFinal.formularios.mantenimientosEmpleado.mantenimientoAdicc
         {
 
         }
+
+        protected void btBuscar_Click(object sender, EventArgs e)
+        {
+            this.contruirReporte();
+        }
+
+        void contruirReporte()
+        {
+
+            ///indicar la ruta del reporte
+            string rutaReporte = "";
+            ///construir la ruta física
+            string rutaServidor = "";
+            ///Validar que la ruta física exista
+            if (!File.Exists(rutaServidor))
+            {
+                this.lblResultado.Text =
+                    "El reporte seleccionado no existe";
+                return;
+            }
+            else
+            {
+                rpvAdiccionesClientes.LocalReport.ReportPath = rutaServidor;
+                var infoFuenteDatos = this.rpvAdiccionesClientes.LocalReport.GetDataSourceNames();
+                ///limpiar los datos de la fuente de datos
+                rpvAdiccionesClientes.LocalReport.DataSources.Clear();
+                ///obtener los datos del reporte
+                List<pa_RetornaAdiccionesxCliente_Result> datosReporte =
+                    null;
+                ///crear la fuente de datos
+                ReportDataSource fuenteDatos = new ReportDataSource();
+                fuenteDatos.Name = infoFuenteDatos[0];
+                fuenteDatos.Value = datosReporte;
+                // agregar la fuente de datos al reporte
+                this.rpvAdiccionesClientes.LocalReport.DataSources.Add(fuenteDatos);
+
+                /// mostrar los datos en el reporte
+                this.rpvAdiccionesClientes.LocalReport.Refresh();
+            }
+        }
+        /// <summary>
+        /// Función que retorna la fuente de datos a mostrar en el reporte
+        /// </summary>
+        /// <param name="pPrimerApellido"></param>
+        /// <param name="pNombre"></param>
+        /// <returns></returns>
+        List<pa_RetornaAdiccionesxCliente_Result> retornaDatosReporte(
+            string pPrimerApellido, string pNombre)
+        {
+            return
+                  null;
+        }
+
     }
 }
--- AseguradoraFinal/AseguradoraFinal/formularios/mantenimientosEmpleado/mantenimientoPoliza/frmPolizaClienteReporte.aspx.cs
+++ AseguradoraFinal/AseguradoraFinal/formularios/mantenimientosEmpleado/mantenimientoPoliza/frmPolizaClienteReporte.aspx.cs
@@ -13,5 +13,58 @@ namespace AseguradoraFinal.formularios.mantenimientosEmpleado.mantenimientoPoliz
         {
 
         }
+
+        protected void btBuscar_Click(object sender, EventArgs e)
+        {
+            this.contruirReporte();
+        }
+
+        void contruirReporte()
+        {
+
+            ///indicar la ruta del reporte
+            string rutaReporte = "";
+            ///construir la ruta física
+            string rutaServidor = "";
+            ///Validar que la ruta física exista
+            if (!File.Exists(rutaServidor))
+            {
+                this.lblResultado.Text =
+                    "El reporte seleccionado no existe";
+                return;
+            }
+            else
+            {
+                rpvPolizaClientes.LocalReport.ReportPath = rutaServidor;
+                var infoFuenteDatos = this.rpvPolizaClientes.LocalReport.GetDataSourceNames();
+                ///limpiar los datos de la fuente de datos
+                rpvPolizaClientes.LocalReport.DataSources.Clear();
+                ///obtener los datos del reporte
+                List< pa_RetornaPoliza_Result > datosReporte =
+                    null;
+                ///crear la fuente de datos
+                ReportDataSource fuenteDatos = new ReportDataSource();
+                fuenteDatos.Name = infoFuenteDatos[0];
+                fuenteDatos.Value = datosReporte;
+                // agregar la fuente de datos al reporte
+                this.rpvPolizaClientes.LocalReport.DataSources.Add(fuenteDatos);
+
+                /// mostrar los datos en el reporte
+                this.rpvPolizaClientes.LocalReport.Refresh();
+            }
+        }
+        /// <summary>
+        /// Función que retorna la fuente de datos a mostrar en el reporte
+        /// </summary>
+        /// <param name="pPrimerApellido"></param>
+        /// <param name="pNombre"></param>
+        /// <returns></returns>
+        List<pa_RetornaPoliza_Result> retornaDatosReporte(
+            string pPrimerApellido, string pNombre)
+        {
+            return
+                  null;
+        }
     }
+
 }
