b8acdb94d26a060024f6fa5954ba6d4971c97de3
RANDALL\randall <randallab_@outlook.com>
2020-12-16 17:50:19 -0600

    remove test hplinks
    remove test hplinks

--- AseguradoraFinal/AseguradoraFinal/formularios/mantenimientosEmpleado/mantenimientoPoliza/frmPolizaLista.aspx.designer.cs
+++ AseguradoraFinal/AseguradoraFinal/formularios/mantenimientosEmpleado/mantenimientoPoliza/frmPolizaLista.aspx.designer.cs
@@ -105,15 +105,6 @@ namespace AseguradoraFinal.formularios.mantenimientosEmpleado.mantenimientoPoliz
         protected global::System.Web.UI.WebControls.HyperLink hpNuevaPoliza;
 
         /// <summary>
-        /// hpPolizaReporte control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hpPolizaReporte;
-
-        /// <summary>
         /// grdListaPoliza control.
         /// </summary>
         /// <remarks>
